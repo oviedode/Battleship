e79fa760c63040f65b3bc3b52e0ef0cbcaa86834
Andrea Rodriguez <amvrodriguez@ucdavis.edu>
2020-01-28 13:13:26 -0800

    self contents

--- Board.py
+++ Board.py
@@ -1,6 +1,13 @@
+from typing import Iterator
 class Board(object):
-    def __init__(self) -> None:
-        super().__init__()
+    def __init__(self, num_rows:int, num_cols:int, blank_char:str) -> None:
+        self.contents = [[blank_char for col in (num_cols)] for row in range(num_rows)]
+        self.blank_char = blank_char
+
+    @property
+    def num_rows(self)-> int:
+        return len(self[0])
+
 
     # Be able to print the board
     def __str__(self) -> str:
@@ -8,6 +15,7 @@ class Board(object):
 
     # Be able to iterate through board
     def __iter__(self):
+        iter(self.contents)
 
     # Get element from board
     def __getitem__(self, index:int):
